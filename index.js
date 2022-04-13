2460d52920cee69c908c5bfb896b7bc7430b84d0
ChrishonCrum <chrishonkcrum@yahoo.com>
2022-04-13 01:37:55 -0500

    commit tree 93abea543667289f45e0c55ab0c4a8b70bebbefb

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+   return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'Accept': "application/json"
+        },
+            body: JSON.stringify({
+                name,
+                email
+              })
+           }) 
+           .then(function (response) {
+            return response.json();
+          })
+          .then(function (object) {
+            console.log(object);
+          });
+    };
+    
+    const apndFunc = document.querySelector('body')
+    apndFunc.append("Some text")
+
+console.log(apndFunc.value) // "Some text"
